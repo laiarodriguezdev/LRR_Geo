65c0ef54b25e18a0b1289f538a51d8f33c9b131e
Laia Rodriguez Ramos <lrodrigu22@jaumebalmes.net>
2024-06-05 21:53:20 +0200

    Acabada

--- js/script.js
+++ js/script.js
@@ -1,79 +1,76 @@
-let meteoritesArray = [];
+let earthMeteorites=[];
 
 fetch("js/json/earthMeteorites.json")
-  .then((response) => response.json())
-  .then((data) => {
-    dades = data.map((meteorit) => {
-      return {
-        name: meteorit.name,
-        lat: meteorit.reclat,
-        long: meteorit.reclong
-      };
-    });
-    printMeteors(dades);
+    .then((response) => response.json())
+    .then((data) => {
+        dades = data.map(meteorit => {
+            return {
+                name: meteorit.name,
+                reclat: meteorit.reclat,
+                reclong: meteorit.reclong
+            };
+        });
+        // console.log(dades);
+        printMeteors(dades);
 });
 
 var map = L.map("map").setView([41.9, 3.17], 11); //PALAMÓS PALAAAAMÓS
-// console.log(map)
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// console.log(map);
+ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map); 
 
-function printMeteors(meteor) {
+function printMeteors(meteors) {
   const resultat = document.getElementById('resultat');
   const taula = document.createElement('table');
 
-  const rowHeader = taula.insertRow();
+  const filaCapcalera = taula.insertRow();
   const titols = ['Nom', 'Latitud', 'Longitud'];
 
-  titols.forEach((titol) => {
+  titols.forEach(titol => {
     const th = document.createElement('th');
     th.textContent = titol;
-    rowHeader.appendChild(th);
+    filaCapcalera.appendChild(th);
   });
 
-  resultat.innerHTML = "";
+  resultat.innerHTML = '';
   resultat.appendChild(taula);
 
-  meteor.forEach((mtr) => {
-    // console.log("entro a fer el taulainsertrow");
+  meteors.forEach(meteor => {
     const row = taula.insertRow();
     const celaNom = row.insertCell(0);
     const celaLat = row.insertCell(1);
     const celaLong = row.insertCell(2);
 
-    celaNom.textContent = mtr.name;
-    celaLat.textContent = mtr.reclat;
-    celaLong.textContent = mtr.reclong;
+    celaNom.textContent = meteor.name;
+    celaLat.textContent = meteor.reclat;
+    celaLong.textContent = meteor.reclong;
 
-    [celaNom, celaLat, celaLong].forEach((cela) => {
-      // console.log("Soc a cela afegint border");
-      cela.style.border = "1px solid black";
-      cela.style.padding = "2.5px";
+    [celaNom, celaLat, celaLong].forEach(cela => {
+      cela.style.border = '3px solid black';
+      cela.style.padding = '5px'; 
     });
 
-    // [celaNom, celaLat, celaLong].forEach(cela => {
-    //   cela.addEventListener('click', () => {
-    //     const lat = parseFloat(mtr.reclat);
-    //     const long = parseFloat(mtr.reclong);
+    [celaNom, celaLat, celaLong].forEach(cela => {
+      cela.addEventListener('click', () => {
+        const lat = parseFloat(meteor.reclat);
+        const long = parseFloat(meteor.reclong);
         
-    //     if (window.marker) {
-    //       window.map.removeLayer(window.marker);
-    //     }
+        if (window.marker) {
+          window.map.removeLayer(window.marker);
+        }
 
-    //     window.marker = L.marker([lat, long]).addTo(window.map);
+        window.marker = L.marker([lat, long]).addTo(window.map);
 
-    //     window.map.setView([lat, long], 15);
+        window.map.setView([lat, long], 15);
 
-    //     const llistaMeteors = `<div style="font-size: 11pt;"><b>Nom:</b> ${mtr.name}<br><b>Latitud:</b> ${mtr.reclat}<br><b>Longitud:</b> ${mtr.reclong}</div>`;
+        const llistaMeteors = `<div style="font-size: 11pt;"><b>Nom:</b> ${meteor.name}<br><b>Latitud:</b> ${meteor.reclat}<br><b>Longitud:</b> ${meteor.reclong}</div>`;
 
-    //     window.marker.bindPopup(llistaMeteors).openPopup();
+        window.marker.bindPopup(llistaMeteors).openPopup();
         
-    //     window.scrollTo(0, 0);
-    //   });
-    // });
-
-    
+        window.scrollTo(0, 0);
+      });
+    });
   });
 }
